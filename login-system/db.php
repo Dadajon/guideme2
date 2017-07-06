4d42dd818ae407b61d3977fbb088c65bbbf16604
Dadajon <dadajonjurakuziev@gmail.com>
2017-07-07 03:45:21 +0900

    added a comment to db.php

--- login-system/db.php
+++ login-system/db.php
@@ -1,7 +1,7 @@
 <?php
 /* Database connection settings */
 $host = 'localhost';
-$user = 'root';
+$user = 'root'; //MacOS password
 $pass = 'root';
 $db = 'accounts';
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
