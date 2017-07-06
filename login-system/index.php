9121b9adc06ea722fa66e6a6720861bace636800
Dadajon <dadajonjurakuziev@gmail.com>
2017-07-07 03:42:05 +0900

    testing Git In Eclipse

--- login-system/index.php
+++ login-system/index.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     if (isset($_POST['login'])) { //user logging in
 
-        require 'login.php';
+        require 'login.php';//just first test
         
     }
     
